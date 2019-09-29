a65ed71fbcbf604517ec524b6f8d25b609e92bad
Jack Dwyer <jdwy215@gmail.com>
2019-09-28 22:23:34 -0400

    adding references to ui elements

--- app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
@@ -2,11 +2,20 @@ package com.foursquare.jdwyer.timefighter
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
+    internal lateinit var tapMeButton: Button
+    internal lateinit var gameScoreTextView: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // once setContentView is loaded, we can use the R file
         setContentView(R.layout.activity_main)
+        tapMeButton = findViewById(R.id.tapMeButton)
+        gameScoreTextView = findViewById(R.id.gameScoreTextView)
     }
 }
