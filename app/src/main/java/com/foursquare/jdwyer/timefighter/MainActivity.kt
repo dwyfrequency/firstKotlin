4b73e97be9a6815e3516dbeed90e425073b5e9e5
Jack Dwyer <jdwy215@gmail.com>
2019-09-29 11:21:35 -0400

    adding resetGame function

--- app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
@@ -2,8 +2,10 @@ package com.foursquare.jdwyer.timefighter
 
 import android.app.Activity
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
 
@@ -12,6 +14,12 @@ class MainActivity : Activity() {
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var gameTimeTextView: TextView
+
+    internal var gameStarted = false
+
+    internal lateinit var countDownTimer: CountDownTimer
+    internal val initialCountDown: Long = 6000
+    internal val countDownInterval: Long = 1000
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,10 +30,34 @@ class MainActivity : Activity() {
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
         gameTimeTextView = findViewById(R.id.TimeLeftTextView)
 
+        gameScoreTextView.text = getString(R.string.yourScore, score)
+        gameTimeTextView.text = getString(R.string.timeLeft, score)
+
         tapMeButton.setOnClickListener {_ ->
             incrementScore()
         }
 
+    }
+
+    private fun resetGame() {
+        score = 0
+        gameScoreTextView.text = getString(R.string.yourScore, score)
+
+
+        val initialTimeLeft = initialCountDown / 1000
+        gameTimeTextView.text = getString(R.string.timeLeft, initialTimeLeft)
+
+        countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
+            override fun onTick(millisUntilFinished: Long) {
+                val timeLeft = millisUntilFinished / 1000
+                gameTimeTextView.text = getString(R.string.timeLeft, timeLeft)
+            }
+
+            override fun onFinish() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+        }
+        gameStarted = false
     }
 
     private fun incrementScore() {
