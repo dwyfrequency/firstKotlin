875cbe195a7a8df5db3e9b77ab99545703e60dfe
Jack Dwyer <jdwy215@gmail.com>
2019-09-28 23:59:50 -0400

    adding clicklist

--- app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
@@ -7,8 +7,11 @@ import android.widget.TextView
 
 class MainActivity : Activity() {
 
+    internal var score = 0
+
     internal lateinit var tapMeButton: Button
     internal lateinit var gameScoreTextView: TextView
+    internal lateinit var gameTimeTextView: TextView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +20,17 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         tapMeButton = findViewById(R.id.tapMeButton)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
+        gameTimeTextView = findViewById(R.id.TimeLeftTextView)
+
+        tapMeButton.setOnClickListener {_ ->
+            incrementScore()
+        }
+
+    }
+
+    private fun incrementScore() {
+        score += 1
+        val newScore = getString(R.string.yourScore, score)
+        gameScoreTextView.text = newScore
     }
 }
