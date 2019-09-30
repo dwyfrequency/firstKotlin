2aca93011f63dfe0e216f14e03e4e38d17ec7957
Jack Dwyer <jdwy215@gmail.com>
2019-09-30 11:17:09 -0400

    adding logging for lifecycle events

--- app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
+++ app/src/main/java/com/foursquare/jdwyer/timefighter/MainActivity.kt
@@ -3,8 +3,10 @@ package com.foursquare.jdwyer.timefighter
 import android.app.Activity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : Activity() {
@@ -20,12 +22,24 @@ class MainActivity : Activity() {
     internal lateinit var countDownTimer: CountDownTimer
     internal val initialCountDown: Long = 60000
     internal val countDownInterval: Long = 1000
+    internal var timeLeftOnTimer: Long = 60000
+
+    // contains property and methods on your class itself
+    // adding this TAG val allows us to easily id where output came from
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+        private const val SCORE_KEY = "SCORE_KEY"
+        private const val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // once setContentView is loaded, we can use the R file
         setContentView(R.layout.activity_main)
+
+        Log.d(TAG, "onCreate called. Score is:  $score")
+
         tapMeButton = findViewById(R.id.tapMeButton)
         gameScoreTextView = findViewById(R.id.gameScoreTextView)
         gameTimeTextView = findViewById(R.id.TimeLeftTextView)
@@ -48,20 +62,49 @@ class MainActivity : Activity() {
 
         countDownTimer = object : CountDownTimer(initialCountDown, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
+                timeLeftOnTimer = millisUntilFinished
                 val timeLeft = millisUntilFinished / 1000
                 gameTimeTextView.text = getString(R.string.timeLeft, timeLeft)
             }
 
             override fun onFinish() {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                endGame()
             }
         }
         gameStarted = false
     }
 
+    private fun startGame() {
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    // Bundle is just a dictionary
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(SCORE_KEY, score)
+        outState?.putLong(TIME_LEFT_KEY, timeLeftOnTimer)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "onSaveInstanceState: Saving Score: $score & Time Left: $timeLeftOnTimer")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy called")
+    }
+
     private fun incrementScore() {
+        if(!gameStarted) {
+            startGame()
+        }
         score += 1
         val newScore = getString(R.string.yourScore, score)
         gameScoreTextView.text = newScore
+    }
+
+    private fun endGame() {
+        Toast.makeText(this, getString(R.string.gameOverMessage, score), Toast.LENGTH_LONG).show()
+        resetGame()
     }
 }
